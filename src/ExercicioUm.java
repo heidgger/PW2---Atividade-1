8ed61567b0b9a494d8c5d8a4a139925b4087fd7b
heidgger <lucas_hen123@hotmail.com>
2020-03-11 20:14:35 -0300

    codificação de método ehDiaSemana()

---
+++ src/ExercicioUm.java
@@ -0,0 +1,13 @@
+
+public class ExercicioUm {
+	public static void main(String[] args) {
+		//tem que ter isso sempre no main
+		// sempre começar com public static
+		System.out.println(ehDiaSemana("segunda"));
+	}
+	public static boolean ehDiaSemana(String dia)
+	{
+		boolean odia =(dia == "segunda" || dia == "terca" || dia == "quarta" || dia == "quinta" || dia == "sexta" || dia == "sabado" || dia == "domingo");
+			return odia;
+		}
+}
